0db965f954c2bdda426a47b17fdcdd0936784d1d
Pasindu Danthaka Sri <pasinduworks92@gmail.com>
2018-05-03 01:51:15 +0930

    jk
    kl

--- backend/app/Http/Controllers/Auth/RegisterController.php
+++ backend/app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\User;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
@@ -51,6 +52,9 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+            'student_id'=>'required|string|max:7',
+            'mobile'=>'required|string|max:10',
+            'type'=>'required|integer|max:1',
         ]);
     }
 
@@ -65,7 +69,11 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => bcrypt($data['password']),
+            'student_id'=>$data['student_id'],
+            'mobile'=>$data['mobile'],
+            'type'=>$data['type'],
+            'device'=>$data['device'],
+            'password' => Hash::make($data['password']),
         ]);
     }
 }
